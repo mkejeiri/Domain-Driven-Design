e450bb5040fad9b0e41746dee03c8252e9f1ed41
Mohamed Larbi Kejeiri <kejeiri@gmail.com>
2019-01-25 11:39:14 +0100

    397magic number explanation

--- SnackMachineDDD.Logic/SnackMachine/SnackPile.cs
+++ SnackMachineDDD.Logic/SnackMachine/SnackPile.cs
@@ -40,6 +40,18 @@ namespace SnackMachineDDD.logic.SnackMachine
 
         protected override int GetHashCodeCore()
         {
+        /******************************************************************************************************************************
+        The value 397 was chosen because it is an odd prime. If it were even and the multiplication overflowed, 
+        information would be lost, as multiplication by 2 is equivalent to shifting. The advantage of using a prime looks like less clear, 
+        and become traditional/obvious through Resharper. To further elaborate on that, SHIFTING left just introduces a zero on the right 
+        and loses a bit on the left of the number's binary representation, so it's a clear information loss. 
+        Repeating this process gradually loses all information that was accumulated from earlier computation. 
+        That means that the more fields enter your hashcode calculation, the less effect on the final result the early fields have. 
+        The objective of hashCode is basically randomizing it across all unequal instances(Overflows are overlooked here). 
+        Multiplication is there to avoid collisions between two fields having commuted values: e.g.
+        if you have int x, y, you don't want the same hashCode when you swap x and y. Multiplication by an odd prime avoid just that.        
+        *******************************************************************************************************************************/
+        
             unchecked
             {
                 int hashCode = Snack.GetHashCode();
