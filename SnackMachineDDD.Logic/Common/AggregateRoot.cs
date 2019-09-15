7bcf82e4b2b4022b800a0bc13da482c6de9b4207
kejeiri <kejeiri@gmail.com>
2019-09-15 13:35:03 +0200

    adding comments[A

--- SnackMachineDDD.Logic/Common/AggregateRoot.cs
+++ SnackMachineDDD.Logic/Common/AggregateRoot.cs
@@ -13,8 +13,16 @@
  *****************************************************************************************************************/
 namespace SnackMachineDDD.logic.Common
 {
+    /*
+     * AggregateRoot has three major goals:
+     * - More readable code base : which entities are AggregateRoot and which ones are part of an existing Aggregate
+     * - Optimistic locking : if it's introduced we need to version the Aggregate
+     * - Perfect place to hold domain events that happens to an Aggregate during its lifetime
+     in our case we will use domain event and optimistic locking so we need otherwise we don't use it.
+     */
     public abstract class AggregateRoot : Entity
     {
+        //Optimistic locking : if it's introduced we need to version the AggregateRoot
         public virtual int Version { get; set; } 
         private readonly List<IDomainEvent> _domainEvents = new List<IDomainEvent>();
         public virtual IReadOnlyList<IDomainEvent> DomainEvents => _domainEvents;
